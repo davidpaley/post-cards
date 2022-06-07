392d6f0ca1e41b980fa585aae2f90888ab0dfed3
davidpaley <david.paleyy@gmail.com>
2022-06-07 16:02:38 -0300

    refactor: delete unnecessary files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import paginationReducer from "../features/pagination/paginationSlice";
 import { postsApi } from "../services/postApi";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     pagination: paginationReducer,
     [postsApi.reducerPath]: postsApi.reducer,
   },
